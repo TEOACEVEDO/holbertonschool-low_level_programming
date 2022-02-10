34f3a954fc957c5f470b14cd852e14392ffd9acd
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-10 09:48:10 -0800

    escribir con puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <studio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-}	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
