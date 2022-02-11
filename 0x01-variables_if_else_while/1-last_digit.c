15aa41fd1cde3ecc230f0e38e71ee66dff6ee39a
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-11 09:27:28 -0800

    mayor que 5 igual a 0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n,z;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+z = n%10;
+if (z > 5)
+{
+printf("%d and is greater than 5");
+}
+else if (z == 0)
+{
+printf("%d and is 0");
+}
+else (z < 6; z != 0)
+{
+printf("%d and is less than 6 and not 0");
+}
+return (0);
+}
