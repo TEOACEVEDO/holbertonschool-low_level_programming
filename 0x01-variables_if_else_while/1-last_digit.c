d16f41586a20d98a4c8efa7dcced0fc364be3aed
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-11 10:39:14 -0800

    ultimo digito

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 z = n % 10;
 if (z > 5)
 {
-printf("the last digit of %i is %i and is greater than 5\n", n , z);
+printf("the last digit of %d is %d and is greater than 5\n", n , z);
 }
 else if (z == 0)
 {
-printf("the last digit of %i is %i and is 0\n", n , z);
+printf("the last digit of %d is %d and is 0\n", n , z);
 }
 else
 {
-printf("the last digit of %i is %i and is less than 6 and not 0\n", n , z);
+printf("the last digit of %d is %d and is less than 6 and not 0\n", n , z);
 }
 return (0);
 }
