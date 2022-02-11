319e50b93cf0246a5b4e5ca816ce6baccca77a68
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-11 10:57:11 -0800

    abecedario

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+putchar(ch);
+
+return (0);
+}
