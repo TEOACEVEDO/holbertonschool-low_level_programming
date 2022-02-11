294a2463318ca1f9c788ba5a11f06fb04b7a290a
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-11 12:17:41 -0800

    del 0 al 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int ch;
+for (ch = '0' ; ch <= '9' ; ch++)
+putchar(ch);
+putchar('\n');
+
+return (0);
+}
