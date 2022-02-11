33838bf533c748d1c2e612f54666ce4e977b4fbb
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-11 12:28:16 -0800

    Alfabeto de z a A

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char ch;
+for (ch = 'z' ; ch <= 'a' ; ch--)
+putchar(ch);
+putchar('\n');
+
+return (0);
+}
