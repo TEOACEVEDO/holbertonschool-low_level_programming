16318c1284fac2b894d355d05bfbed743ef05b79
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-11 13:11:44 -0800

    del o al 9 con , y espacio

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,15 @@
 int main(void)
 {
 int n;
-for (n = '0'; n <= '9'; n++)
+for (n = '0'; n <= '9')
 {
 putchar(n);
 if (n != '9')
+{
 putchar(',');
 putchar(' ');
+}
+n++;
 }
 putchar('\n');
 return (0);
