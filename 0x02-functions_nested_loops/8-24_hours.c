4e955e976e71e344592f2cb1854c090b0e4138ed
TEOACEVEDO <98199846+TEOACEVEDO@users.noreply.github.com>
2022-02-15 11:44:51 -0500

    Horas y minutos

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 /**
  *jack_bauer - print every minute of a day
  * Description: print minutes 00:00 to 23:59
- * 
+ *
  */
 void jack_bauer(void)
 {
@@ -11,7 +11,7 @@ int min = 0;
 
 while (hours < 24)
 {
-while (min <60)
+while (min < 60)
 {
 	_putchar('0' + (hours / 10));
 			_putchar('0' + (hours % 10));
