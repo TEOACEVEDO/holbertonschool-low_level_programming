21071753505dc296d8fdb035449a0c99da8e72ad
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-15 10:40:32 -0800

    integrales

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - a function that adds two integers and returns the result
+ * @perrito: first number input
+ * @gatico: second number input
+ * Return: perrito + gatico
+ */
+int add(int perrito, int gatico)
+{
+    return (perrito + gatico);
+}
