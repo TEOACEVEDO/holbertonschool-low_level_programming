fad28d289929c6ff23979251cfcbe0a507d8c191
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-15 06:51:53 -0800

    imprimir 10 veces el alfabeto

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - Entry point
+ *print_alphabet_x10 - esto sirve para imprimirlo 10 veces 
  *
- * Return: Always 0 (Success)
+ * 
  */
 void print_alphabet_x10(void)
 {
