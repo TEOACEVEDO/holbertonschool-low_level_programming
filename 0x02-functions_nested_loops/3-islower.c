f685f2a4ba502c31455032035544516dc02df562
TEOACEVEDO <98199846+TEOACEVEDO@users.noreply.github.com>
2022-02-15 10:31:01 -0500

    El 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,14 +6,13 @@
  */
 int _islower(int c)
 {
-
-	if (c >= 'a' && c <= 'z')
-	{
-	return (1);
-	}
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
 else
 {
-		return (0);
+return (0);
 }
 _putchar('\n');
 }
