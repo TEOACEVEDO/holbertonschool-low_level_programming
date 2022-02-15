d555f6f832b0382ab76839626eff1b5d91b91fc4
TEOACEVEDO <98199846+TEOACEVEDO@users.noreply.github.com>
2022-02-15 10:42:01 -0500

    +-

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *print_sign - function
- *@al: variable of the function
+ *@n: variable of the function
  *Return: Todo depende de la funcion
  */
 int print_sign(int n);
@@ -19,7 +19,7 @@ else if (n < 0)
 }
 else 
 {
--putchar('0');
+_putchar('0');
 return (0);
 }
 _putchar('\n') 
