539ecfeffe8b648ae2d106b86ce6c289fec9d2f4
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-17 12:07:37 -0800

    linea recta

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_line - Escribe una lina recta
+* @n: variable cantidad de lineas rectas
+*/
+void print_line(int n)
+{
+	char c;
+
+	for (c = 0; c < n; c+1)
+		_putchar('_');
+	_putchar('\n');
+}
