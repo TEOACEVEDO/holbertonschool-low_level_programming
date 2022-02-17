9925ad75159ba20cd145b998dd26b7c7d561902d
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-17 12:46:13 -0800

    cuadrado de #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_square - nos muestra un cuadrado
+* @size: variabl del tamaño
+*/
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
