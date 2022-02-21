311b7abac485850caf561d67380c6513dc7d7d09
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-21 11:48:29 -0800

    Cadena y longitud

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* swap_int - Esta funcion sirve para intercambiar
-* @a: es la variable de entrada
-* @b: es la variable de entrada
+* _strlen - prototipo para la cadena 
+* @s: es la variable de entrada
 * Return: 0
 */
 int _strlen(char *s)
 {
-char *str;
-int len;
+    int longitud = 0;
 
-str = "My first strlen!";
-len = _strlen(str);
+    while (*s != '\0')
+    {
+        longitud++;
+        s++;
+    }
+
+    return (longitud);
 }
