14dda7c586a4689e13e981ca629856e6b4d274a2
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-21 14:59:50 -0800

    la mitad

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* puts2 - nos muestre caracteres que sean numeros enteros haciendo % 2
+* @str: Input string
+**/
+void puts_half(char *str);
+{
+int i, n, len;
+
+len = 0;
+
+for (i = 0; str[i] != '\0'; i++)
+        len++;
+
+n = (len / 2);
+
+if ((len % 2) == 1)
+n = ((len + 1) / 2);
+
+for (i = n; str[i] != '\0'; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 
 #endif
