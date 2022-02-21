9694351da3e1cd60bcc81db3f126d628d8eea6d6
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-21 11:30:31 -0800

    Cadena

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* swap_int - Esta funcion sirve para intercambiar
+* @a: es la variable de entrada
+* @b: es la variable de entrada
+* Return: 0
+*/
+int _strlen(char *s)
+{
+char *str;
+int len;
+
+str = "My first strlen!";
+len = _strlen(str);
+}
