89cbc51a1e1c4aefd02ea549bcc9f5ad6d53748b
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-21 10:34:13 -0800

    pointer 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* @n: es la variable de entrada 
-* reset_to_98 - prototipo 
+* @n: es la variable de entrada
+* reset_to_98 - prototipo
 * Return: 0
 */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 int n = 98;
 int *p = &n;
