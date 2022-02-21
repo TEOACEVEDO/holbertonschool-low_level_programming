b190f8036197f1ed23743d4381805fb6b07baa10
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-21 10:55:51 -0800

    intercambiar

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* swap_int - Esta funcion sirve para intercambiar
+* @a: es la variable de entrada
+* @b: es la variable de entrada
+* Return: 0
+*/
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
