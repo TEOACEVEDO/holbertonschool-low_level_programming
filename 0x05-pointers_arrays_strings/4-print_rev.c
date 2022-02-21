89c38014799fe44b2a751bef7f7c8642fffbf6e6
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-21 12:30:32 -0800

    imprimir cadena alreves

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_rev - prototipo para la cadena
+* @s: es la variable de entrada
+*/
+void print_rev(char *s);
+{
+
+while (*s != '\0')
+{
+_putchar(*s--);
+}
+_putchar('\n');
+}
