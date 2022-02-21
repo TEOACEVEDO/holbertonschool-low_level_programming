9c736b7aa6e75b0114e72710f5620fe69e6b56c1
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-21 14:41:21 -0800

    resultados enteros sin residuos %2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,23 +6,23 @@
 **/
 void puts2(char *str)
 {
-    int len = 0;
-    int l = 0;
-    char y = *str;
-    int z;
+int len = 0;
+int l = 0;
+char *y = str;
+int z;
 
-    while (y != '\0')
-    {
-        y++;
-        len++;
-    }
-    l = len - 1;
-    for (z = 0 ; z <= l ; z++)
-    {
-        if (z % 2 == 0)
-        {
-            _putchar(*str[z]);
-        }
-    }
-    _putchar('\n');
+while (*y != '\0')
+{
+y++;
+len++;
+}
+l = len - 1;
+for (z = 0 ; z <= l ; z++)
+{
+if (z % 2 == 0)
+{
+_putchar(str[z]);
+}
+}
+_putchar('\n');
 }
