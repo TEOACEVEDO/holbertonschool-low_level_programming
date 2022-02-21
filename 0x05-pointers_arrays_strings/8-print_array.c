02bd26ff876ed9bfc35d2f7e1c78068582704c6c
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-21 15:13:44 -0800

    Matriz

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_array - update value.
+* @a: value to be evaluate.
+* @n: value to be evaluate.
+**/
+void print_array(int *a, int n)
+{
+int x;
+
+for (x = 0 ; x < n; x++)
+{
+printf("%d", a[x]);
+if (x != n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
