b19f6015189a3e079d66360e280890577b18933c
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-28 11:34:43 -0800

    igualar una string a otra

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,9 @@
 #include "holberton.h"
 /**
- *
+ *_strpbrk - searching a string for any of a set of bytes
+ * @s: the string
+ * @accept: set of bytes
+ * Return: a pointer to the byte in s that matches one of the bytes in accept
  */
 char *_strpbrk(char *s, char *accept)
 {
