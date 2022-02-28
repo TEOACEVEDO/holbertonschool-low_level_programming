d8dc2cb31eba9b874277374e83e1ac8da0b5d974
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-02-28 12:58:34 -0800

    tablero 8x8

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/*
+/**
  * print_chessboard - Entry point
  * @a: array
  * Return: Always 0 (Success)
@@ -11,9 +11,9 @@ void print_chessboard(char (*a)[8])
 for (t = 0; t < 8; t++)
 {
 for (s = 0; s < 8; s++)
-{		
+		
 	_putchar(a[t][s])
-}
+
 	_putchar('\n');
 }
 }
