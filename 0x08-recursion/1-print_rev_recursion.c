34ce197e74b305f17a52fa94646d0feda7dba829
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-03-02 10:58:17 -0800

    cadena en rever

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - prints a string, followed by a new line
+ *@s: pointer to string
+ *Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+	_print_rev_recursion(s --s);
+	_putchar(*s);
+	}
+	
+}
