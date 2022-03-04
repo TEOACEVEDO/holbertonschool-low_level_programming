afab00e4808f0dba59935bac10c6e14837a02ebd
TEOACEVEDO <98199846+TEOACEVEDO@users.noreply.github.com>
2022-03-04 14:10:19 -0500

    sumar numeros positivos

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,23 +12,23 @@
  */
 int main(int argc, char *argv[])
 {
-    int num, digit, sum = 0;
+int num, digit, sum = 0;
 
-    for (num = 1; num < argc; num++)
-    {
-        for (digit = 0; argv[num][digit]; digit++)
-        {
-            if (argv[num][digit] < '0' || argv[num][digit] > '9')
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
+for (num = 1; num < argc; num++)
+{
+for (digit = 0; argv[num][digit]; digit++)
+{
+if (argv[num][digit] < '0' || argv[num][digit] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
 
-        sum += atoi(argv[num]);
-    }
+sum += atoi(argv[num]);
+}
 
-    printf("%d\n", sum);
+printf("%d\n", sum);
 
-    return (0);
+return (0);
 }
