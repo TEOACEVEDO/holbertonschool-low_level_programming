bca42288bd8d5940e79fac4632ff73f6e7d03129
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-03-04 10:24:32 -0800

    multiplacion

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - multiply two numbers
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: result of multiplication or 1
+ */
+int main(int argc, char *argv[])
+{
+	int t, a, result;
+
+if(argc < 2)
+{	
+printf("error\n");
+return (1);
+}	
+/* atoi sirve para convertir una string a un int */
+else
+t = atoi(argv[1]);
+a = atoi(argv[2]);
+result = t * a;
+printf("%d\n", result);
+return (0);
+}
