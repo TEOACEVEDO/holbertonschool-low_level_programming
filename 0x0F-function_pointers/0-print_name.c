6d52d5c5c485f5d5ec39e1c0f7a1d345aa556ef8
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-03-17 13:09:21 -0700

    imprime nombre

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdlib.h>
+#include <stdio.h>
 /**
  * print_name - execute a function given as a parameter
  * @name: parameter that function ptr needs
@@ -9,7 +9,7 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (!name && !f)
 	{
-		;
+		return ;
 	}
 	f(name);
 }
