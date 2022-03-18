c02a6289d37de27a0f3c96291b23beb47b2f1466
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-03-18 12:33:13 -0700

    sumar todos los argumentos

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,22 @@
 #ifndef VARIADICFUNC
 #define VARIADICFUNC
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct dt - struct
+ * @letter: letter signifying data type
+ * @func: function ptr
+ */
+typedef struct dt
+{
+	char letter;
+	void (*func)(va_list);
+} datatype;
+
 
 #endif
