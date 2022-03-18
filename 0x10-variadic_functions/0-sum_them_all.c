7f8c846e4d6f3f9b4352ff2e752fe744009d4467
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-03-18 11:41:05 -0700

    sumar todos los argumentos

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ *sum_them_all - sum all arguements
+ * @n: number of arguements
+ * Return: 0 is n is 0 or return sum
+ *
+ */
+int sum_them_all(const unsigned int n, ...);
+{
+	int sum;
+	va_list numbers;
+	unsigned int i;
+
+if (n == 0)
+return (0);
+
+	va_list numbers;
+	int sum;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+	sum = 0;
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(numbers, int);
+	}
+	va_end(numbers);
+	return (sum);
+}
