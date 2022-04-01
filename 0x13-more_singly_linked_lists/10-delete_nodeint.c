81b261a73ac52d4d6263b8c78a9b75999e115184
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-04-01 09:28:47 -0700

    borrar nodo especifico

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * delete_nodeint_at_index - deletes the node at index 
+ * delete_nodeint_at_index - deletes the node at index
  * @head: head of the list
  * @index: emplacement of the node to delet
  * Return: pointr to head of list
