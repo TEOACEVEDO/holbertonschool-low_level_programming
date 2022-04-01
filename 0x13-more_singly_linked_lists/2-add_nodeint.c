8f09d66778bf28a8d9fa6d199f730d9071d181dc
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-04-01 08:02:56 -0700

    añadir nodo al comienzo

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,11 +14,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	if (new != NULL){
 	new->n = n;
 	new->next = *head;
 	*head = new;
 	return (*head);
-	}
-
 }
