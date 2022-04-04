612b460a835c339e6142b41f07642c2f3ebc765b
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-04-04 13:58:51 -0700

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,45 +1,28 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
+
 /**
- *binary_to_uint - Converts a binary number to an unsigned int
- *@b: The binary string to converts
- *Return: the positive number converted from a binary 
+ * binary_to_uint -  converts a binary number to an unsigned int.
+ * @b: char
+ *
+ * Return:  the converted number.
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len = 0, count = 0, sum = 0;
+	unsigned int val = 0;
+	int i = 0;
 
-	if (b == NULL)
+	if (b == NULL || b == 0)
 		return (0);
 
-	len = _strlen(b);
-	while (len--)
-	{
-		if (b[len] != 48 && b[len] != 49)
+	while (b[i] != '\0')
+	{ /*Found another digit.*/
+		if (b[i] != '1' && b[i] != '0')
 			return (0);
-
-		if (b[len] == 49)
-			sum += 1 << count;
-
-		count++;
+		val <<= 1;
+		val += b[i] - '0';
+		i++;
 	}
-
-	return (sum);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
+	return (val);
 }
