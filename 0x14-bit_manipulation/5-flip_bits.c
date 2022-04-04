9af523ac494d8a0b125d79bd4b4811ae3dd01b3d
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-04-04 14:46:51 -0700

    bits que necesitaria cambiar parra pasar de numero

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits you would need to flip to get from one number to another
+ * flip_bits - returns the number of bits
+ * you would need to flip to get from one number to another
  * @n: number one.
  * @m: number two.
  *
