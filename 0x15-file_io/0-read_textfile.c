b77270f23e64aa7cab24943dd61067e62028ce82
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-04-05 10:40:11 -0700

    funcion cp

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * read_textfile - Write a function that reads a text file and prints it to the POSIX standard output.
- * @filename: File to read and print.
- * @letters: Number of letters it should read and print.
- * Return: The actual number of letters it could read and print
+ *read_textfile - Write a function that reads a text
+ *file and prints it to the POSIX standard output.
+ *@filename: File to read and print.
+ *@letters: Number of letters it should read and print.
+ *Return: The actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
