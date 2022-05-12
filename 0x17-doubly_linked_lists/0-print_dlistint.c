b0d35e9f181b6c4d069c0ce05c20c9e5adcda4ba
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-05-12 10:59:15 -0700

    return the number of elements in a linked

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,7 +8,7 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int counter = 0;
+	int count = 0;
 
 	if (h == NULL)
 		return (0);
@@ -17,7 +17,7 @@ size_t print_dlistint(const dlistint_t *h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		counter++;
+		count++;
 	}
-	return (counter);
+	return (count);
 }
