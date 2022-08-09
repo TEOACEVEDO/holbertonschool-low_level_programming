133b28f8b0fca36c0b625dddec00d3978bd7793a
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-08-09 12:04:06 -0500

    binary search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function that is used for extract a information
+ * @array:  is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: the value
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i <= size; i++)
+	{
+		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
